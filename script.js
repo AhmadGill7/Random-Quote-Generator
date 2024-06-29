b58d0ebfa1543339f045f8536a96773dc14682d5
Muhammad Ahmad <162286232+AhmadGill7@users.noreply.github.com>
2024-06-29 10:33:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const generate = () => {
+
+
+ 
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: "https://api.quotable.io/quotes/random",
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key":
+                "47fad9205amshaabf4694159358fp1b7854jsne35ba97a7c0e",
+            "X-RapidAPI-Host": "timshim-quotes-v1.p.rapidapi.com",
+        },
+    };
+
+    $.ajax(settings).done(function (response) {
+        // Assuming response is an array of quotes
+        if (response[0].length > 0) {
+            // const randomQuote = response[Math.floor(Math.random() * response.length)];
+            document.getElementById("quote").innerHTML = response[0].content;
+            document.getElementById("author").innerHTML = "- " + response[0].author;
+        }
+    });
+}
+generate()
